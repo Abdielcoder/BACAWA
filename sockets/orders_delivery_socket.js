df14ff1921e1839a33e190757006969c65448add
Red Skull <redkullcoder@gmail.com>
2022-06-28 18:04:04 +0000

    BACAWA19 - ADDING DATA TO SOCKECT STATUS

--- sockets/orders_delivery_socket.js
+++ sockets/orders_delivery_socket.js
@@ -4,6 +4,7 @@ module.exports = (io) => {
     const orderAsigned = io.of('/orders/asigned');
     const orderStatus = io.of('/orders/status');
     const latLngwasher=io.of('/orders/lat');
+    const orderscatch=io.of('/orders/catch');
 
     orderDeliveryNamespace.on('connection', function(socket) {
 
@@ -44,7 +45,7 @@ module.exports = (io) => {
 
         socket.on('status', function(data) {
             console.log(`EMITIO STATUS ${JSON.stringify(data)}`);
-            orderStatus.emit(`status/${data.id_order}`, { statusOrder: data.statusOrder, lat: data.lat , lng: data.lng  });
+            orderStatus.emit(`status/${data.id_order}`, { statusOrder: data.statusOrder, lat: data.lat , lng: data.lng, idClient: data.idClient, idAddress: data.idAddress  });
         });
 
         socket.on('disconnect', function(data) {
@@ -60,6 +61,22 @@ latLngwasher.on('connection', function(socket) {
         socket.on('lat', function(data) {
             console.log(`EMITIO LA POSICION ${JSON.stringify(data)}`);
             latLngwasher.emit(`lat/${data.id_order}`, { lat: data.lat, lng: data.lng  });
+        });
+
+        socket.on('disconnect', function(data) {
+            console.log('USUARIO DEJO DE EMITIR COORDENADAS');
+        });
+    });
+
+
+orderscatch.on('connection', function(socket) {
+
+
+        console.log('USUARIO CONECTADO  /orders/catch');
+
+        socket.on('orderscatch', function(data) {
+            console.log(`EMITIO LA POSICION ${JSON.stringify(data)}`);
+            latLngwasher.emit(`orderscatch`, { idClient: data.client ,idAddress: data.address, lat: data.lat, lng: data.lng  });
         });
 
         socket.on('disconnect', function(data) {
